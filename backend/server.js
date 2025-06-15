2d99b35067ee9fd9f10d07fc8d55f7b9c911b712
Linkin143 <kumarprince143p143@gmail.com>
2025-06-16 04:03:46 +0530

    session updated

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import session from 'express-session';
 import passport from 'passport';
 import connectDB from "./config/db.js";
 import './config/passportConfig.js'; // This must come before you use passport
@@ -24,12 +23,7 @@ app.use(passport.initialize());
 
 
 
-app.use(session({
-    secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true, maxAge: 3600000 }
-}));
+
 
 const port = process.env.PORT || 5000;
 
