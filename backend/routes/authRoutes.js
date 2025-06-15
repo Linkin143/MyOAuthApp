c2e192a95dd908a0e1ef1e982427f250c414cba6
Linkin143 <kumarprince143p143@gmail.com>
2025-06-16 03:28:59 +0530

    local routes removed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -22,12 +22,12 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get('/google/callback', 
   passport.authenticate('google', {
     session: false,
-    failureRedirect: 'http://localhost:5173' || "https://oauthweb.netlify.app"
+    failureRedirect: "https://oauthweb.netlify.app"
   }),
   (req, res) => {
     const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
     res.cookie('TOKEN', token, { httpOnly: true, maxAge: 3600000 });
-    res.redirect('http://localhost:5173/dashboard' || "https://oauthweb.netlify.app/dashboard");
+    res.redirect("https://oauthweb.netlify.app/dashboard");
   }
 );
 
